d7d7efc9afe1ab478f5625145bb70b2613e9d2f0
lht <145413385+SeasonofBlossoms@users.noreply.github.com>
2025-09-11 18:00:30 +0800

    feat: 暂存

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import "../style/common.module.scss";
 import "./page.scss";
 import Image from "next/image";
 import Link from "next/link";
+import PickaxeIcon from "@/icons/pickaxe.svg";
 export default function Home() {
   return (
     <div className="home">
@@ -166,6 +167,35 @@ export default function Home() {
                 />
               </div>
               <div className="picture_mask"></div>
+            </div>
+            <div className="right_content">
+              <h2 className="font_serif">关于我</h2>
+              <p className="my_descrption">
+                你好，我是一名热爱技术与生活的开发者。白天，我是一名前端工程师，专注于构建流畅、美观的用户界面；夜晚，我喜欢阅读、写作和探索世界。
+              </p>
+              <p className="blog_descrption">
+                这个博客是我记录思考的空间，在这里你可以看到我对技术的探索、对书籍的感悟、对生活的思考以及旅行中的见闻。我相信分享是最好的学习方式，希望这里的内容能给你带来一些启发。
+              </p>
+              <div className="technology_hobby flex">
+                <div className="technology">
+                  <h3 className="fac">
+                    <div className="type_icon fajc">
+                      <Image
+                        width={14}
+                        height={14}
+                        src={PickaxeIcon}
+                        unoptimized
+                        alt="nuknow"
+                      />
+                    </div>
+                    技术领域
+                  </h3>
+                  <ul>
+                    <li>前端开发 (React, Vue)</li>
+                  </ul>
+                </div>
+                <div className="hobby"></div>
+              </div>
             </div>
           </div>
         </div>
