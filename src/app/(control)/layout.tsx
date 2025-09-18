c13e9af8c0afacde1e564adb2d5c089d970a49cb
lht <145413385+SeasonofBlossoms@users.noreply.github.com>
2025-09-18 20:42:53 +0800

    feat: 布局

--- src/app/(control)/layout.tsx
+++ src/app/(control)/layout.tsx
@@ -31,7 +31,7 @@ export default function RootControlLayoutRootLayout({
   }));
   return (
     <div className="control">
-      <Layout>
+      <Layout style={{ height: "100%" }}>
         <Header style={{ display: "flex", alignItems: "center" }}>
           <div className="demo-logo" />
           <Menu
@@ -42,14 +42,12 @@ export default function RootControlLayoutRootLayout({
             style={{ flex: 1, minWidth: 0 }}
           />
         </Header>
-        <div style={{ padding: "0 48px" }}>
+        <Layout style={{ padding: "0 48px 12px" }}>
           <Breadcrumb
-            style={{ margin: "16px 0" }}
             items={[{ title: "Home" }, { title: "List" }, { title: "App" }]}
           />
           <Layout
             style={{
-              padding: "24px 0",
               background: colorBgContainer,
               borderRadius: borderRadiusLG,
             }}
@@ -65,11 +63,9 @@ export default function RootControlLayoutRootLayout({
                 items={menus}
               />
             </Sider>
-            <Content style={{ padding: "0 24px", minHeight: 280 }}>
-              {children}
-            </Content>
+            <Content style={{ padding: "0 24px" }}>{children}</Content>
           </Layout>
-        </div>
+        </Layout>
       </Layout>
     </div>
   );
